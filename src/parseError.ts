6476b96c3914d2d00d936a96b4d912634c0b3887
Guy Segal <guysegal.il@gmail.com>
2022-02-20 07:53:10 +0200

    .

--- src/parseError.ts
+++ src/parseError.ts
@@ -12,7 +12,7 @@ import {
 } from './errors/axios';
 
 export function assertError(e: any): asserts e is Error {
-  return e.message;
+  return e.message && e.name && e.stack;
 }
 
 export const parseError = async (e: AxiosError | Error | Response): Promise<any> => {
