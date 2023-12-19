b6dc5d04334f4ebb884dd35289dd9f86fd05a7ce
Itamar Kiesler <99740936+ItamarKieslerFabric@users.noreply.github.com>
2023-12-19 10:59:00 +0200

    Update dependencies (#6)

--- src/parseError.ts
+++ src/parseError.ts
@@ -1,15 +1,6 @@
-import type { Response } from 'node-fetch';
 import type { AxiosError } from 'axios';
-import {
-  isError as isFetchError,
-  assertError as assertFetchError,
-  parseError as parseFetchError,
-} from './errors/fetch.js';
-import {
-  isError as isAxiosError,
-  assertError as assertAxiosError,
-  parseError as parseAxiosError,
-} from './errors/axios.js';
+import { isError as isFetchError, assertError as assertFetchError, parseError as parseFetchError } from './errors/fetch.js';
+import { isError as isAxiosError, assertError as assertAxiosError, parseError as parseAxiosError } from './errors/axios.js';
 
 export function assertError(e: any): asserts e is Error {
   return e.name && e.message;
